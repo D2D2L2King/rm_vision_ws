bd426bb26d487d9a9253cbb129ec5cfbb7351d52
H1R0 <2468449282@qq.com>
2025-08-04 02:04:17 +0800

    normal

--- src/traditional_vision/include/process.h
+++ src/traditional_vision/include/process.h
@@ -5,7 +5,7 @@
 #include "sensor_msgs/Image.h" // ROS图像消息头文件
 #include "geometry_msgs/Point.h" // ROS几何消息头文件
 #include <mutex> // 互斥锁头文件
-
+#include <vector> // 向量头文件
 
 cv::Mat image2cv(const sensor_msgs::ImageConstPtr& msg); // 图像转换函数声明
 cv::Mat image_processing(const cv::Mat& image); // 图像处理函数声明
